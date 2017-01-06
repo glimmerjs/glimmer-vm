663fcf111cb4d7d29c7cdc85bbe25dc9c2161f44
Jacek Bandura <jacek.bandura@netguru.pl>
2017-01-06 19:47:00 +0100

    Convert Walker to class syntax

--- packages/@glimmer/syntax/lib/traversal/walker.ts
+++ packages/@glimmer/syntax/lib/traversal/walker.ts
@@ -1,28 +1,3 @@
-function Walker(order=undefined) {
-  this.order = order;
-  this.stack = [];
-}
-
-export default Walker;
-
-Walker.prototype.visit = function(node, callback) {
-  if (!node) {
-    return;
-  }
-
-  this.stack.push(node);
-
-  if (this.order === 'post') {
-    this.children(node, callback);
-    callback(node, this);
-  } else {
-    callback(node, this);
-    this.children(node, callback);
-  }
-
-  this.stack.pop();
-};
-
 let visitors = {
   Program: function(walker, node, callback) {
     for (let i = 0; i < node.body.length; i++) {
@@ -42,9 +17,34 @@ let visitors = {
   }
 };
 
-Walker.prototype.children = function(node, callback) {
-  let visitor = visitors[node.type];
-  if (visitor) {
-    visitor(this, node, callback);
+export default class Walker {
+  constructor(order = undefined) {
+    this.order = order;
+    this.stack = [];
   }
-};
+
+  visit(node, callback) {
+    if (!node) {
+      return;
+    }
+
+    this.stack.push(node);
+
+    if (this.order === 'post') {
+      this.children(node, callback);
+      callback(node, this);
+    } else {
+      callback(node, this);
+      this.children(node, callback);
+    }
+
+    this.stack.pop();
+  }
+
+  children(node, callback) {
+    let visitor = visitors[node.type];
+    if (visitor) {
+      visitor(this, node, callback);
+    }
+  }
+}
