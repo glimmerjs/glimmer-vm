07798ea1cfd4990649d0857e67dda0a6fa152de3
Godfrey Chan <godfreykfc@gmail.com>
2017-06-22 11:04:12 -0700

    --amend

--- packages/@glimmer/runtime/lib/opcode-builder.ts
+++ packages/@glimmer/runtime/lib/opcode-builder.ts
@@ -12,7 +12,7 @@ import * as WireFormat from '@glimmer/wire-format';
 import { Block } from './syntax/interfaces';
 
 import { PublicVM } from './vm/append';
-import { Resolver } from "@glimmer/interfaces";
+import { Resolver, Specifier } from "@glimmer/interfaces";
 
 export type ComponentArgs = [WireFormat.Core.Params, WireFormat.Core.Hash, Option<Block>, Option<Block>];
 
@@ -26,6 +26,6 @@ export interface DynamicComponentDefinition {
 }
 
 export interface ComponentBuilder {
-  static(definition: ComponentDefinition<Opaque>, args: ComponentArgs): void;
+  static(definition: Specifier, args: ComponentArgs): void;
   dynamic(definitionArgs: ComponentArgs, getDefinition: DynamicComponentDefinition, args: ComponentArgs): void;
 }
