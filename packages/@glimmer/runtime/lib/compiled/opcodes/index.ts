dfb018d22a75c9e098dfd3a1ca815fa9644ddda1
Chad Hietala <chadhietala@gmail.com>
2017-08-22 17:00:50 -0400

    I hate phantomjs

--- packages/@glimmer/runtime/lib/compiled/opcodes/index.ts
+++ packages/@glimmer/runtime/lib/compiled/opcodes/index.ts
@@ -7,19 +7,20 @@ import { EXPRESSION_MAPPINGS } from './expressions';
 import { LIST_MAPPINGS } from './lists';
 import { PARTIAL_MAPPINGS } from './partial';
 import { VM_MAPPINGS } from './vm';
+import { assign } from "@glimmer/util";
 
 export const operations = new Array(Op.Size);
 
-let operationsMap = {
-  ...COMPONENT_MAPPINGS,
-  ...CONTENT_MAPPINGS,
-  ...DEBUG_MAPPINGS,
-  ...DOM_MAPPINGS,
-  ...EXPRESSION_MAPPINGS,
-  ...LIST_MAPPINGS,
-  ...PARTIAL_MAPPINGS,
-  ...VM_MAPPINGS
-};
+let operationsMap = assign({},
+  COMPONENT_MAPPINGS,
+  CONTENT_MAPPINGS,
+  DEBUG_MAPPINGS,
+  DOM_MAPPINGS,
+  EXPRESSION_MAPPINGS,
+  LIST_MAPPINGS,
+  PARTIAL_MAPPINGS,
+  VM_MAPPINGS
+);
 
 for (let key in operationsMap) {
   operations[key] = operationsMap[key];
