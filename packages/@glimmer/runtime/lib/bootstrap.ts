6de2bad10a33ae3c325ddde46f2bed12174bba8c
Chad Hietala <chadhietala@gmail.com>
2017-08-22 16:04:50 -0400

    remove most of the bootstrapping

--- packages/@glimmer/runtime/lib/bootstrap.ts
+++ packages/@glimmer/runtime/lib/bootstrap.ts
@@ -1,9 +1,1 @@
 import './opcodes';
-import './compiled/opcodes/expressions';
-import './compiled/opcodes/component';
-import './compiled/opcodes/content';
-import './compiled/opcodes/debugger';
-import './compiled/opcodes/dom';
-import './compiled/opcodes/partial';
-import './compiled/opcodes/vm';
-import './compiled/opcodes/lists';
