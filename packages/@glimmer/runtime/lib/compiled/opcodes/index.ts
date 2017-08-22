4eb368b114cf5ee795c61c598c564b96302c08a0
Chad Hietala <chadhietala@gmail.com>
2017-08-22 16:38:51 -0400

    Fix type errors

--- packages/@glimmer/runtime/lib/compiled/opcodes/index.ts
+++ packages/@glimmer/runtime/lib/compiled/opcodes/index.ts
@@ -7,14 +7,6 @@ import { EXPRESSION_MAPPINGS } from './expressions';
 import { LIST_MAPPINGS } from './lists';
 import { PARTIAL_MAPPINGS } from './partial';
 import { VM_MAPPINGS } from './vm';
-export * from './component';
-export * from './content';
-export * from './debugger';
-export * from './dom';
-export * from './lists';
-export * from './expressions';
-export * from './partial';
-export * from './vm';
 
 export const operations = new Array(Op.Size);
 
@@ -29,6 +21,6 @@ let operationsMap = {
   ...VM_MAPPINGS
 };
 
-Object.keys(operationsMap).map(t => parseInt(t, 10)).forEach((op: number) => {
-  operations[op] = operationsMap[op];
-});
+for (let key in operationsMap) {
+  operations[key] = operationsMap[key];
+}
