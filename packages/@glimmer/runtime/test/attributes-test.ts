b098cd327ad124ef3d445d8c252bb98f96828e94
Sergio Arbeo <serabe@gmail.com>
2017-10-06 13:19:29 +0200

    Update tests

--- packages/@glimmer/runtime/test/attributes-test.ts
+++ packages/@glimmer/runtime/test/attributes-test.ts
@@ -1,5 +1,5 @@
 import { RenderTest, module, test } from '@glimmer/test-helpers';
-import { SVG_NAMESPACE, normalizeProperty } from "../index";
+import { SVG_NAMESPACE, normalizeProperty } from '@glimmer/runtime';
 
 class AttributesTests extends RenderTest {
 
@@ -263,6 +263,28 @@ class AttributesTests extends RenderTest {
 
     this.rerender({ name: '' });
     this.assert.equal(this.readDOMAttr('value'), '');
+    this.assertStableNodes();
+  }
+
+  @test "handles empty string input placeholders"() {
+    this.render('<input type="text" placeholder={{name}} />', { name: '' });
+    this.assert.equal(this.readDOMAttr('placeholder'), '');
+    this.assertStableRerender();
+
+    this.rerender({ name: 'Alex' });
+    this.assert.equal(this.readDOMAttr('placeholder'), 'Alex');
+
+    this.rerender({ name: '' });
+    this.assert.equal(this.readDOMAttr('placeholder'), '');
+    this.assertStableNodes();
+  }
+
+  @test "type attribute can be set to non-valid type"() {
+    this.render('<input type="yolo" />');
+    this.assert.equal(this.readDOMAttr('type'), 'text');
+    this.assertStableRerender();
+
+    this.rerender();
     this.assertStableNodes();
   }
 
@@ -555,347 +577,3 @@ module("Attribute img[src]", class extends BoundValuesToSpecialAttributeTests {
   protected isEmptyElement = true;
   protected isSelfClosing = false;
 });
-/*
-test("div[href] is not not marked as unsafe", () => {
-  let template = compile('<div href="{{foo}}"></div>');
-
-  let context = { foo: 'javascript:foo()' };
-  render(template, context);
-
-  equalTokens(root, '<div href="javascript:foo()"></div>');
-
-  rerender();
-
-  equalTokens(root, '<div href="javascript:foo()"></div>');
-});
-
-test("triple curlies in attribute position", () => {
-
-  let template = compile('<div data-bar="bar" data-foo={{{rawString}}}>Hello</div>');
-
-  render(template, { rawString: 'TRIPLE' });
-
-  equalTokens(root, '<div data-foo="TRIPLE" data-bar="bar">Hello</div>', "initial render");
-
-  rerender({ rawString: 'DOUBLE' });
-
-  equalTokens(root, '<div data-foo="DOUBLE" data-bar="bar">Hello</div>', "initial render");
-});
-
-test('can read attributes', assert => {
-  let template = compile('<div data-bar="bar"></div>');
-
-  render(template);
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'data-bar'), 'bar');
-});
-
-test('can read attributes from namespace elements', assert => {
-  let template = compile('<svg viewBox="0 0 0 0"></svg>');
-
-  render(template);
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'viewBox'), '0 0 0 0');
-});
-
-test('can read properties', assert => {
-  let template = compile('<input value="gnargnar" />');
-
-  render(template);
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'gnargnar');
-});
-
-test('handles null input values', assert => {
-  let template = compile('<input value={{isNull}} />');
-
-  render(template, { isNull: null });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-
-  rerender({ isNull: 'hey' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'hey');
-
-  rerender({ isNull: null });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-});
-
-test('handles undefined input values', assert => {
-  let template = compile('<input value={{isUndefined}} />');
-
-  render(template, { isUndefined: undefined });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-
-  rerender({ isUndefined: 'hey' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'hey');
-
-  rerender({ isUndefined: undefined });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-});
-
-test('handles undefined `toString` input values', assert => {
-  let obj = Object.create(null);
-  let template = compile('<input value={{obj}} />');
-
-  render(template, { obj });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-
-  rerender({ obj: 'hello' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'hello');
-
-  rerender({ obj });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-});
-
-test('input[checked] prop updates when set to undefined', assert => {
-  let template = compile('<input checked={{if foo true undefined}} />');
-
-  env.registerHelper('if', (params) => {
-    if (params[0]) {
-      return params[1];
-    } else {
-      return params[2];
-    }
-  });
-
-  render(template, { foo: true });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), true);
-
-  rerender({ foo: false });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), false);
-
-  rerender({ foo: true });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), true);
-});
-
-test('input[checked] prop updates when set to null', assert => {
-  let template = compile('<input checked={{foo}} />');
-
-  render(template, { foo: true });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), true);
-
-  rerender({ foo: null });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), false);
-
-  rerender({ foo: true });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'checked'), true);
-});
-
-test('select[value] prop updates when set to undefined', assert => {
-  let template = compile('<select value={{foo}}><option></option><option value="us" selected>us</option></select>');
-
-  // setting `select[value]` only works after initial render, just use `undefined` here but it doesn't really matter
-  render(template, { foo: undefined });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'us');
-
-  // now setting the `value` property will have an effect
-  rerender({ foo: null });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-
-  rerender({ foo: 'us' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'us');
-});
-
-test('handles empty string textarea values', assert => {
-  let template = compile('<textarea value={{name}} />');
-
-  render(template, { name: '' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-
-  rerender({ name: 'Alex' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), 'Alex');
-
-  rerender({ name: '' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'value'), '');
-});
-
-test('handles empty string input placeholders', assert => {
-  let template = compile('<input type="text" placeholder={{name}} />');
-
-  render(template, { name: '' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'placeholder'), '');
-
-  rerender({ name: 'Alex' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'placeholder'), 'Alex');
-
-  rerender({ name: '' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'placeholder'), '');
-});
-
-test('type attribute can be set to non-valid type', assert => {
-  let template = compile('<input type="yolo" />');
-
-  render(template, { type: 'yolo' });
-
-  assert.equal(readDOMAttr(root.firstChild as Element, 'type'), 'text');
-});
-
-test('does not set undefined attributes', assert => {
-  let template = compile('<div data-foo={{isUndefined}} /><div data-foo={{isNotUndefined}} />');
-
-  render(template, { isUndefined: undefined, isNotUndefined: 'hello' });
-
-  let firstElement = root.firstChild as Element;
-  let secondElement = root.lastChild as Element;
-
-  assert.ok(!firstElement.hasAttribute('data-foo'));
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-
-  rerender({ isUndefined: 'hey', isNotUndefined: 'hello' });
-
-  assert.ok(firstElement.hasAttribute('data-foo'));
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-
-  rerender({ isUndefined: 'hey', isNotUndefined: 'world' });
-
-  assert.ok(firstElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(firstElement, 'data-foo'), 'hey');
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'world');
-
-  rerender({ isUndefined: undefined, isNotUndefined: 'hello' });
-
-  assert.ok(!firstElement.hasAttribute('data-foo'));
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-});
-
-test('does not set null attributes', assert => {
-  let template = compile('<div data-foo={{isNull}} /><div data-foo={{isNotNull}}></div>');
-
-  render(template, { isNull: null, isNotNull: 'hello' });
-
-  let firstElement = root.firstChild as Element;
-  let secondElement = root.lastChild as Element;
-
-  assert.ok(!firstElement.hasAttribute('data-foo'));
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-
-  rerender({ isNull: 'hey', isNotNull: 'hello' });
-
-  assert.ok(firstElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(firstElement, 'data-foo'), 'hey');
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-
-  rerender({ isNull: 'hey', isNotNull: 'world' });
-
-  assert.ok(firstElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(firstElement, 'data-foo'), 'hey');
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'world');
-
-  rerender({ isNull: null, isNotNull: 'hello' });
-
-  assert.ok(!firstElement.hasAttribute('data-foo'));
-  assert.ok(secondElement.hasAttribute('data-foo'));
-  assert.equal(readDOMAttr(secondElement, 'data-foo'), 'hello');
-});
-
-test('does not set undefined properties initially', assert => {
-  let template = compile('<div title={{isUndefined}} /><div title={{isNotUndefined}}></div>');
-
-  render(template, { isUndefined: undefined, isNotUndefined: 'hello' });
-
-  let firstElement = root.firstChild as Element;
-  let secondElement = root.lastChild as Element;
-
-  assert.ok(!firstElement.hasAttribute('title'));
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  equalTokens(root, '<div></div><div title="hello"></div>');
-
-  rerender({ isUndefined: 'hey', isNotUndefined: 'hello' });
-
-  assert.equal(readDOMAttr(firstElement, 'title'), 'hey');
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  equalTokens(root, '<div title="hey"></div><div title="hello"></div>');
-
-  rerender({ isUndefined: 'hey', isNotUndefined: 'world' });
-
-  assert.equal(readDOMAttr(firstElement, 'title'), 'hey');
-  assert.equal(readDOMAttr(secondElement, 'title'), 'world');
-  equalTokens(root, '<div title="hey"></div><div title="world"></div>');
-
-  rerender({ isUndefined: undefined, isNotUndefined: 'hello' });
-
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  assert.equal(readDOMAttr(firstElement, 'title'), '');
-  assert.equal(readDOMAttr(firstElement, 'title'), nativeValueForElementProperty('div', 'title', ''));
-  equalTokens(root, '<div></div><div title="hello"></div>');
-});
-
-test('does not set null properties initially', assert => {
-  let template = compile('<div title={{isNull}} /><div title={{isNotNull}}></div>');
-
-  render(template, { isNull: null, isNotNull: 'hello' });
-
-  let firstElement = root.firstChild as Element;
-  let secondElement = root.lastChild as Element;
-
-  assert.ok(!firstElement.hasAttribute('title'));
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  equalTokens(root, '<div></div><div title="hello"></div>');
-
-  rerender({ isNull: 'hey', isNotNull: 'hello' });
-
-  assert.equal(readDOMAttr(firstElement, 'title'), 'hey');
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  equalTokens(root, '<div title="hey"></div><div title="hello"></div>');
-
-  rerender({ isNull: 'hey', isNotNull: 'world' });
-
-  assert.equal(readDOMAttr(firstElement, 'title'), 'hey');
-  assert.equal(readDOMAttr(secondElement, 'title'), 'world');
-  equalTokens(root, '<div title="hey"></div><div title="world"></div>');
-
-  rerender({ isNull: null, isNotNull: 'hello' });
-
-  assert.equal(readDOMAttr(secondElement, 'title'), 'hello');
-  assert.equal(readDOMAttr(firstElement, 'title'), '');
-  assert.equal(readDOMAttr(firstElement, 'title'), nativeValueForElementProperty('div', 'title', ''));
-  equalTokens(root, '<div></div><div title="hello"></div>');
-});
-
-test("input list attribute updates properly", () => {
-  let template = compile('<input list="{{foo}}" />');
-
-  let context = { foo: "bar" };
-  render(template, context);
-
-  equalTokens(root, '<input list="bar" />');
-
-  rerender({ foo: "baz" });
-
-  equalTokens(root, '<input list="baz" />');
-
-  rerender({ foo: "bar" });
-
-  equalTokens(root, '<input list="bar" />');
-});
-*/
