a594cfde0ec2b80ae9c192880c18827ebfb3ec23
Alex Crichton <alex@alexcrichton.com>
2017-12-20 09:27:54 -0800

    Comment used unstable features

--- packages/@glimmer/low-level/rust/src/lib.rs
+++ packages/@glimmer/low-level/rust/src/lib.rs
@@ -1,4 +1,14 @@
-#![feature(link_llvm_intrinsics, proc_macro)]
+// This unstable feature is required for the `page` module which accesses the
+// raw wasm memory allocation instructions for now. Eventually we may move
+// to libstd allocations but for now it's intended to be an allocator with a
+// smaller code-size footprint as in theory it's not necessary for this
+// appplication to need a full-blown allocator.
+#![feature(link_llvm_intrinsics)]
+
+// This unstable feature is currently used for the `wasm_bindgen` macro which
+// allows us to define Rust code and have TypeScript automatically generated
+// for consumption elsewhere.
+#![feature(proc_macro)]
 
 // We're optimizing for code size, so don't accidentally use any libstd
 // abstractions
