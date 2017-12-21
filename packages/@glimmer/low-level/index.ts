ce5454e1994087fb7098fa6ab494bdb3bac4c32f
Alex Crichton <alex@alexcrichton.com>
2017-12-21 12:57:29 -0800

    Remove unused bits from low-level package

--- packages/@glimmer/low-level/index.ts
+++ packages/@glimmer/low-level/index.ts
@@ -1,5 +1,4 @@
 import { wasm, booted } from './lib/rust-wrapper';
 import { LowLevelVM as WasmLowLevelVM } from './lib/rust';
 
-export * from './lib/glue/storage';
 export { wasm, booted, WasmLowLevelVM };
