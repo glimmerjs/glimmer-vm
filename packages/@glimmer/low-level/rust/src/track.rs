0f7b70c71531d42472a11b60ccd3be40be805f2a
Alex Crichton <alex@alexcrichton.com>
2018-01-19 15:44:21 -0800

    Only track allocations and such in debug mode

--- packages/@glimmer/low-level/rust/src/track.rs
+++ packages/@glimmer/low-level/rust/src/track.rs
@@ -6,17 +6,25 @@ pub struct Tracked(());
 
 impl Tracked {
     pub fn new() -> Tracked {
-        NUM.fetch_add(1, Ordering::SeqCst);
+        if cfg!(debug_assertions) {
+            NUM.fetch_add(1, Ordering::SeqCst);
+        }
         Tracked(())
     }
 }
 
 impl Drop for Tracked {
     fn drop(&mut self) {
-        NUM.fetch_sub(1, Ordering::SeqCst);
+        if cfg!(debug_assertions) {
+            NUM.fetch_sub(1, Ordering::SeqCst);
+        }
     }
 }
 
 pub fn total() -> usize {
-    NUM.load(Ordering::SeqCst)
+    if cfg!(debug_assertions) {
+        NUM.load(Ordering::SeqCst)
+    } else {
+        0
+    }
 }
