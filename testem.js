7789d59fa58df3eb4c28e9d6ad474845634890fa
Chad Hietala <chadhietala@gmail.com>
2019-01-17 09:58:20 -0500

    Widen operand slot to 32-bits
    (cherry picked from commit 944e7885b82d9591ed0ae15763639c70aa5215d5)
    
    Co-authored-by: Kris Selden <kris.selden@gmail.com>

--- testem.js
+++ testem.js
@@ -1,23 +1,28 @@
 'use strict';
 
 let isCI = !!process.env.CI;
+let smokeTests = !!process.env.SMOKE_TESTS
 
 let config = {
   "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
+  "test_page": smokeTests ? "tests/index.html?smoke_tests=true" : "tests/index.html?hidepassed",
   "disable_watching": true,
+  "browser_start_timeout": smokeTests ? 300000 : 30000,
   "launchers": {
     "Node": {
       "command": "./bin/run-node-tests.js",
       "protocol": "tap"
      }
   },
+  "timeout":  smokeTests ? 300000 : 30000,
   "browser_args": {
     "Chrome": {
       "mode": "ci",
       "args": [
         '--disable-gpu',
+        '—-timeout=300000',
         '--headless',
+        '--no-sandbox',
         '--remote-debugging-port=9222'
       ]
     }
