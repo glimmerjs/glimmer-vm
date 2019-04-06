emberjs/ember.js

RFC #435: Forward element modifiers with splattributes

packages/@glimmer/wire-format/index.ts
@@ -8,14 +8,15 @@ export function is<T>(variant: number): (value: any) => value is T {
 
 // Statements
 export const isFlushElement = is<Statements.FlushElement>(SexpOpcodes.FlushElement);
-export const isAttrSplat = is<Statements.AttrSplat>(SexpOpcodes.AttrSplat);
 
 export function isAttribute(val: Statement): val is Statements.Attribute {
   return (
     val[0] === SexpOpcodes.StaticAttr ||
     val[0] === SexpOpcodes.DynamicAttr ||
     val[0] === SexpOpcodes.TrustingDynamicAttr ||
-    val[0] === SexpOpcodes.ComponentAttr
+    val[0] === SexpOpcodes.ComponentAttr ||
+    val[0] === SexpOpcodes.AttrSplat ||
+    val[0] === SexpOpcodes.Modifier
   );
 }
 

glimmerjs/glimmer-vm

packages/@glimmer/wire-format/index.ts
@@ -1,7 +1,6 @@
 import { Dict, Option, Opaque } from '@glimmer/util';
 import { Opcodes } from './lib/opcodes';
-
-export { Opcodes as Ops } from './lib/opcodes';
+export { Opcodes as Ops };
 
 type JsonValue = string | number | boolean | JsonObject | JsonArray;
 
@@ -238,7 +237,8 @@ export function isAttribute(val: Statement): val is Statements.Attribute {
     val[0] === Opcodes.ComponentAttr ||
     val[0] === Opcodes.TrustingAttr ||
     val[0] === Opcodes.TrustingComponentAttr ||
-    val[0] === Opcodes.AttrSplat
+    val[0] === Opcodes.AttrSplat ||
+    val[0] === Opcodes.Modifier
   );
 }
 