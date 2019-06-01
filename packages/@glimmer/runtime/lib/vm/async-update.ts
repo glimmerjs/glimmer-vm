c3031b55a149a5525f8e5f8819113975ac93836f
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-06-01 21:32:49 +0300

    basic ie11 support

--- packages/@glimmer/runtime/lib/vm/async-update.ts
+++ packages/@glimmer/runtime/lib/vm/async-update.ts
@@ -8,6 +8,7 @@ interface Deadline {
 }
 
 type IdleCallback = (deadline: Deadline) => void;
+type PromiseCallbck = (result?: any) => void;
 
 export interface AsyncRendererOptions {
   timeout?: number;
@@ -21,31 +22,74 @@ declare function requestIdleCallback(
   options?: RequestIdleCallbackOptions
 ): void;
 
+const useFallback = typeof requestIdleCallback !== 'function';
+const tickExecutor = !useFallback
+  ? function(cb: IdleCallback, opts: RequestIdleCallbackOptions) {
+      return requestIdleCallback(cb, opts);
+    }
+  : function(cb: IdleCallback) {
+      return setTimeout(cb);
+    };
+// base IE11 support without transpilation
+function happyPromiseLike(cb: PromiseCallbck): Promise<void> {
+  if (typeof Promise !== 'undefined') {
+    return new Promise(cb);
+  }
+  const ctx: {
+    tasks: PromiseCallbck[];
+    then(cb: PromiseCallbck): void;
+  } = {
+    tasks: [],
+    then(cb: PromiseCallbck) {
+      this.tasks.push(cb);
+    },
+  };
+  setTimeout(() => {
+    cb(() => {
+      ctx.tasks.forEach(task => task());
+    });
+  });
+  return (ctx as unknown) as Promise<void>;
+}
 export default class AsyncUpdatingVM extends UpdatingVM {
   public timeout = 20;
   public tickSize = 50;
-  execute(opcodes: UpdatingOpSeq, handler: ExceptionHandler): Promise<void> {
+  public tickExecutor = tickExecutor;
+  public useFallback = useFallback;
+  public tickTime!: number;
+  scheduleTick(tick: IdleCallback, options: RequestIdleCallbackOptions) {
+    this.tickExecutor(tick, { timeout: options.timeout } as any);
+  }
+  haveTime(deadline: Deadline | undefined) {
+    if (this.useFallback) {
+      return Date.now() - this.tickTime < this.timeout;
+    } else {
+      return (deadline as Deadline).timeRemaining() > 1;
+    }
+  }
+  execute(opcodes: UpdatingOpSeq, handler: ExceptionHandler): Promise<void> | void {
     this.setup(opcodes, handler);
-    return new Promise(resolve => {
+    return happyPromiseLike(resolve => {
       let timeout = this.timeout;
       let tick = (deadline: Deadline) => {
         let iteratorResult = false;
+        this.tickTime = Date.now();
         do {
           let decrement = this.tickSize;
           do {
             iteratorResult = this.next();
             decrement--;
           } while (iteratorResult && decrement > 0);
-        } while (iteratorResult && deadline.timeRemaining() > 1);
+        } while (iteratorResult && this.haveTime(deadline));
 
         if (!iteratorResult) {
           return resolve();
         }
 
-        requestIdleCallback(tick, { timeout });
+        this.scheduleTick(tick, { timeout });
       };
 
-      requestIdleCallback(tick, { timeout });
+      this.scheduleTick(tick, { timeout });
     });
   }
 }
