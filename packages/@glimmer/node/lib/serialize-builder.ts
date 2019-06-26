9c0ec8aa45c13436bfd1adaa9d5c46a88e717a0c
Jarek Radosz <jradosz@gmail.com>
2019-06-26 19:27:20 +0200

    Don’t emit rehydration comments for style elements
    (cherry picked from commit 5108b17b4896c9cd866ccf46aec945b90a20a6cd)

--- packages/@glimmer/node/lib/serialize-builder.ts
+++ packages/@glimmer/node/lib/serialize-builder.ts
@@ -30,7 +30,7 @@ class SerializeBuilder extends NewElementBuilder implements ElementBuilder {
   __openBlock(): void {
     let { tagName } = this.element;
 
-    if (tagName !== 'TITLE' && tagName !== 'SCRIPT') {
+    if (tagName !== 'TITLE' && tagName !== 'SCRIPT' && tagName !== 'STYLE') {
       let depth = this.serializeBlockDepth++;
       this.__appendComment(`%+b:${depth}%`);
     }
@@ -43,7 +43,7 @@ class SerializeBuilder extends NewElementBuilder implements ElementBuilder {
 
     super.__closeBlock();
 
-    if (tagName !== 'TITLE' && tagName !== 'SCRIPT') {
+    if (tagName !== 'TITLE' && tagName !== 'SCRIPT' && tagName !== 'STYLE') {
       let depth = --this.serializeBlockDepth;
       this.__appendComment(`%-b:${depth}%`);
     }
@@ -52,7 +52,7 @@ class SerializeBuilder extends NewElementBuilder implements ElementBuilder {
   __appendHTML(html: string): Bounds {
     let { tagName } = this.element;
 
-    if (tagName === 'TITLE' || tagName === 'SCRIPT') {
+    if (tagName === 'TITLE' || tagName === 'SCRIPT' || tagName === 'STYLE') {
       return super.__appendHTML(html);
     }
 
@@ -81,7 +81,7 @@ class SerializeBuilder extends NewElementBuilder implements ElementBuilder {
     let { tagName } = this.element;
     let current = currentNode(this);
 
-    if (tagName === 'TITLE' || tagName === 'SCRIPT') {
+    if (tagName === 'TITLE' || tagName === 'SCRIPT' || tagName === 'STYLE') {
       return super.__appendText(string);
     } else if (string === '') {
       return (this.__appendComment('% %') as any) as Simple.Text;
