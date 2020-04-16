9383695600ef8b2e5baf3bb9c9b806041f469ec7
Robert Jackson <rjackson@linkedin.com>
2020-04-16 10:38:01 -0400

    Backport: Properly cleanup the element builder in a try/finally
    This is a backport of the changes in a5fefa3 to the release-0-38-alpha
    branch so that we can fix these issues in ember-source@3.16 (which still
    uses 0.38-alpha).

--- packages/@glimmer/test-helpers/lib/suites/components.ts
+++ packages/@glimmer/test-helpers/lib/suites/components.ts
@@ -773,4 +773,96 @@ export class BasicComponents extends RenderTest {
     this.assertHTML(`<div color='red'>hello!</div>`);
     this.assertStableNodes();
   }
+
+    @test({ kind: 'fragment' })
+  'throwing an error during component construction does not put result into a bad state'() {
+    this.registerComponent(
+      'Glimmer',
+      'Foo',
+      'Hello',
+      class extends EmberishGlimmerComponent {
+        constructor() {
+          super();
+          throw new Error('something went wrong!');
+        }
+      }
+    );
+
+    this.render('{{#if showing}}<Foo/>{{/if}}', {
+      showing: false,
+    });
+
+    this.assert.throws(() => {
+      this.rerender({ showing: true });
+    }, 'something went wrong!');
+
+    this.assertHTML('<!---->', 'values rendered before the error rendered correctly');
+    this.destroy();
+
+    this.assertHTML('', 'destroys correctly');
+  }
+
+  @test({ kind: 'fragment' })
+  'throwing an error during component construction does not put result into a bad state with multiple prior nodes'() {
+    this.registerComponent(
+      'Glimmer',
+      'Foo',
+      'Hello',
+      class extends EmberishGlimmerComponent {
+        constructor() {
+          super();
+          throw new Error('something went wrong!');
+        }
+      }
+    );
+
+    this.render('{{#if showing}}<div class="first"></div><div class="second"></div><Foo/>{{/if}}', {
+      showing: false,
+    });
+
+    this.assert.throws(() => {
+      this.rerender({ showing: true });
+    }, 'something went wrong!');
+
+    this.assertHTML(
+      '<div class="first"></div><div class="second"></div><!---->',
+      'values rendered before the error rendered correctly'
+    );
+    this.destroy();
+
+    this.assertHTML('', 'destroys correctly');
+  }
+
+  @test({ kind: 'fragment' })
+  'throwing an error during component construction does not put result into a bad state with nested components'() {
+    this.registerComponent(
+      'Glimmer',
+      'Foo',
+      'Hello',
+      class extends EmberishGlimmerComponent {
+        constructor() {
+          super();
+          throw new Error('something went wrong!');
+        }
+      }
+    );
+
+    this.registerComponent('Basic', 'Bar', '<div class="second"></div><Foo/>');
+
+    this.render('{{#if showing}}<div class="first"></div><Bar/>{{/if}}', {
+      showing: false,
+    });
+
+    this.assert.throws(() => {
+      this.rerender({ showing: true });
+    }, 'something went wrong!');
+
+    this.assertHTML(
+      '<div class="first"></div><div class="second"></div><!---->',
+      'values rendered before the error rendered correctly'
+    );
+    this.destroy();
+
+    this.assertHTML('', 'destroys correctly');
+  }
 }
