132527f5e30840e4f92acdc6c37b3b7e88d60890
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-08 18:30:34 -0400

    Bah

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,6 +3,16 @@ import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin';
 import { defaultReporter } from '@web/test-runner';
 // import { browserstackLauncher } from '@web/test-runner-browserstack';
 import { webdriverLauncher } from '@web/test-runner-webdriver';
+// import { createServer } from 'vite';
+
+// let server = await createServer({ server: { hmr: false } });
+// await server.listen();
+
+const vitePort = 4444;
+// const vitePort = server.config.server.port;
+// const viteProtocol = server.config.server.https ? 'https' : 'http';
+// const path = `${viteProtocol}://0.0.0.0:${vitePort}`;
+// console.log(`\t${path}`);
 
 // const sharedCapabilities = {
 //   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
@@ -16,12 +26,14 @@ export default {
   // We point at a lone file because we don't want @web/test-runner
   // to actually do anything with `files`, but `files` is a required config option.
   files: ['testem.js'],
+  port: vitePort,
   // Launches Vite
   plugins: [vitePlugin()],
   reporters: [defaultReporter({ reportTestProgress: true, reportTestResults: true })],
   browsers: [
     webdriverLauncher({
       automationProtocol: 'webdriver',
+      port: vitePort,
       path: '/',
       capabilities: {
         browserName: 'chrome',
@@ -29,7 +41,7 @@ export default {
           args: [
             // --no-sandbox is needed when running Chrome inside a container
             process.env.CI ? '--no-sandbox' : null,
-            '--headless',
+            '--headless=new',
             '--disable-gpu',
             '--disable-dev-shm-usage',
             '--disable-software-rasterizer',
