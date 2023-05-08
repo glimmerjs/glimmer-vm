7face822ddc01c2ae23dcac70dd6684fe899f138
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-08 15:09:30 -0400

    More config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,8 +1,59 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin';
+import { defaultReporter } from '@web/test-runner';
+// import { browserstackLauncher } from '@web/test-runner-browserstack';
+import { webdriverLauncher } from '@web/test-runner-webdriver';
+
+// const sharedCapabilities = {
+//   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+//   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
+//   project: '',
+//   name: 'glimmer-vm',
+//   build: `build ${process.env.GITHUB_RUN_NUMBER || 'unknown'}`,
+// };
 
 export default {
-  // For watching
-  files: './packages/@glimmer/*/test/**/*-test.ts',
+  // @web/test-runner does not decide what tests to run
+  files: ['**/*.ts'],
   // Launches Vite
   plugins: [vitePlugin()],
+  reporters: [defaultReporter({ reportTestProgress: true, reportTestResults: true })],
+  browsers: [
+    webdriverLauncher({
+      automationProtocol: 'webdriver',
+      path: '/',
+      capabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: [
+            // --no-sandbox is needed when running Chrome inside a container
+            process.env.CI ? '--no-sandbox' : null,
+            '--headless',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-software-rasterizer',
+            '--mute-audio',
+            '--remote-debugging-port=0',
+            '--window-size=1440,900',
+          ].filter(Boolean),
+        },
+      },
+    }),
+    webdriverLauncher({
+      automationProtocol: 'webdriver',
+      path: '/',
+      capabilities: {
+        browserName: 'firefox',
+        'moz:firefoxOptions': {
+          args: ['-headless'],
+        },
+      },
+    }),
+    // browserstackLauncher({
+    //   ...sharedCapabilities,
+    //   browserName: 'Chrome',
+    //   os: 'Windows',
+    //   os_version: '10',
+    // }),
+  ],
 };
