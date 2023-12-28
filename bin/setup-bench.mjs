97ebcd1a4e97198227e3c64d123b82dfc03c6f66
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-28 12:18:10 -0500

    Update the benchmark config

--- bin/setup-bench.mjs
+++ bin/setup-bench.mjs
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import 'zx/globals';
 import os from 'node:os';
 import { join } from 'node:path';
@@ -58,27 +59,24 @@ const tempDir = os.tmpdir();
 const CONTROL_DIR = join(tempDir, 'control');
 const EXPERIMENT_DIR = join(tempDir, 'experiment');
 
-const CONTROL_BENCH_DIR = join(CONTROL_DIR, 'benchmark', 'benchmarks', 'krausest');
-const EXPERIMENT_BENCH_DIR = join(EXPERIMENT_DIR, 'benchmark', 'benchmarks', 'krausest');
-
-const pwdRaw = await $`pwd`;
-const pwd = pwdRaw.toString().trim();
+const pwd = process.cwd();
 
 // we use benchmark from current commit, very useful if we need to tweak it
 const benchmarkFolder = 'benchmark';
+const krausest = 'benchmark/benchmarks/krausest';
 
 // remove node_modules from benchmark folder, maybe we could figure out better option to distribute bench source
 await $`rm -rf ${join(pwd, benchmarkFolder, 'node_modules')}`;
-await $`rm -rf ${join(pwd, benchmarkFolder, 'benchmarks', 'krausest', 'node_modules')}`;
+await $`rm -rf ${join(pwd, benchmarkFolder, 'benchmarks/krausest/node_modules')}`;
 
 await $`rm -rf ${CONTROL_DIR}`;
 await $`rm -rf ${EXPERIMENT_DIR}`;
 await $`mkdir ${CONTROL_DIR}`;
 await $`mkdir ${EXPERIMENT_DIR}`;
 
-const isMacOs = os.platform() === 'darwin';
-
 const BENCHMARK_FOLDER = join(pwd, benchmarkFolder);
+const BIN_FOLDER = join(pwd, 'bin');
+const WORKSPACE_FOLDER = join(pwd, 'packages/@glimmer-workspace');
 
 const rawUpstreamUrl = await $`git ls-remote --get-url upstream`;
 const rawOriginUrl = await $`git ls-remote --get-url origin`;
@@ -102,58 +100,92 @@ const EXPERIMENT_URL = `http://localhost:${EXPERIMENT_PORT}`;
 
 // we can't do it in parallel on CI,
 
-// setup experiment
-await within(async () => {
-  await cd(EXPERIMENT_DIR);
-  await $`git clone ${originUrlStr} .`;
-  await $`git checkout ${experimentBranchName}`;
-  await $`rm -rf ./benchmark`;
-  await $`cp -r ${BENCHMARK_FOLDER} ./benchmark`;
+/**
+ * Setup the experiment / control with the exact same
+ * - benchmark folder
+ * - bin folder
+ *
+ * (as what is on the experiment branch)
+ */
+async function setup({ targetDir, remoteUrl, branchName, label }) {
+  await within(async () => {
+    cd(targetDir);
 
-  console.info('installing experiment source');
-  await $`pnpm install --no-frozen-lockfile`.quiet();
-  console.info('building experiment source, may take a while');
-  await $`pnpm build`.quiet();
+    await $`git clone ${remoteUrl} .`;
+    await $`git checkout ${branchName}`;
+    await $`rm -rf ./bin`;
+    await $`cp -r ${BIN_FOLDER} ${targetDir}/bin`;
 
-  if (isMacOs) {
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"main": "index.ts",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"main": "./dist/index.js",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"import": "./dist/index.js"|"import": "./dist/prod/index.js"|g' {} \\;`;
-  } else {
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"main": "index.ts",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"main": "./dist/index.js",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"import": "./dist/index.js"|"import": "./dist/prod/index.js"|g' {} \\;`;
-  }
+    console.info(`installing ${label} source`);
+    await $`pnpm install --no-frozen-lockfile`.quiet();
+    console.info(`building ${label} source, may take a while`);
+    await $`pnpm build`.quiet();
+    await $`pnpm esyes ${targetDir}/bin/preview-publish.mts`;
 
-  await cd(EXPERIMENT_BENCH_DIR);
-  await $`pnpm vite build`;
+    cd(join(targetDir, 'dist'));
+
+    await $`mkdir -p ${targetDir}/dist/@glimmer-workspace/`;
+    await $`cp -r ${BENCHMARK_FOLDER} ${targetDir}/dist/benchmark`;
+    await $`cp -r ${WORKSPACE_FOLDER}/benchmark-env ${targetDir}/dist/@glimmer-workspace/benchmark-env`;
+    await $`echo "  - '@glimmer-workspace/*'" >> pnpm-workspace.yaml`;
+    await $`echo "  - 'benchmark'" >> pnpm-workspace.yaml`;
+    await $`echo "  - 'benchmark/benchmarks/krausest'" >> pnpm-workspace.yaml`;
+    await $`cd ${targetDir}/dist && pnpm install --no-frozen-lockfile`;
+
+    cd(join(targetDir, 'dist', krausest));
+
+    await fs.writeFile(
+      join(targetDir, 'dist', krausest, 'tsconfig.json'),
+      `
+        {
+          "compilerOptions": {
+            "composite": true,
+            "strict": true,
+            "baseUrl": ".",
+            "allowJs": true,
+            "checkJs": true,
+
+            "target": "es2020",
+            "module": "esnext",
+            "moduleResolution": "bundler",
+            "verbatimModuleSyntax": true,
+            "noErrorTruncation": true,
+
+            "suppressImplicitAnyIndexErrors": false,
+            "useDefineForClassFields": false,
+            "exactOptionalPropertyTypes": true,
+            "noImplicitOverride": true,
+            "noPropertyAccessFromIndexSignature": true,
+            "noUncheckedIndexedAccess": true,
+            "skipLibCheck": true,
+            "noEmit": true,
+            "paths": {
+              "@/components/*": ["./lib/components/*"],
+              "@/utils/*": ["./lib/utils/*"],
+              "@glimmer-workspace/benchmark-env": ["../../../@glimmer-workspace/benchmark-env"],
+              "@glimmer/runtime": ["../../../@glimmer/runtime"]
+            }
+          },
+          "include": ["./lib/", "./browser.js"],
+        }
+    `
+    );
+    await $`pnpm vite build`;
+  });
+}
+
+await setup({
+  label: 'experiment',
+  targetDir: EXPERIMENT_DIR,
+  remoteUrl: originUrlStr,
+  branchName: experimentBranchName,
 });
 
-// setup control
-await within(async () => {
-  await cd(CONTROL_DIR);
-  await $`git clone ${upstreamUrlStr} .`;
-  await $`git checkout ${controlBranchName}`;
-  await $`rm -rf ./benchmark`;
-  await $`cp -r ${BENCHMARK_FOLDER} ./benchmark`;
-
-  console.info('installing control source');
-  await $`pnpm install --no-frozen-lockfile`.quiet();
-  console.info('building control source, may take a while');
-  await $`pnpm build`.quiet();
-
-  if (isMacOs) {
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"main": "index.ts",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"main": "./dist/index.js",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i '' 's|"import": "./dist/index.js"|"import": "./dist/prod/index.js"|g' {} \\;`;
-  } else {
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"main": "index.ts",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"main": "./dist/index.js",|"main": "./dist/prod/index.js","module": "./dist/prod/index.js",|g' {} \\;`;
-    await $`find ./packages -name 'package.json' -exec sed -i 's|"import": "./dist/index.js"|"import": "./dist/prod/index.js"|g' {} \\;`;
-  }
-
-  await cd(CONTROL_BENCH_DIR);
-  await $`pnpm vite build`;
+await setup({
+  label: 'control',
+  targetDir: CONTROL_DIR,
+  remoteUrl: upstreamUrlStr,
+  branchName: controlBranchName,
 });
 
 console.info({
@@ -164,8 +196,8 @@ console.info({
 });
 
 // start build assets
-$`cd ${CONTROL_BENCH_DIR} && pnpm vite preview --port ${CONTROL_PORT}`;
-$`cd ${EXPERIMENT_BENCH_DIR} && pnpm vite preview --port ${EXPERIMENT_PORT}`;
+$`cd ${join(CONTROL_DIR, 'dist', krausest)} && pnpm vite preview --port ${CONTROL_PORT}`;
+$`cd ${join(EXPERIMENT_DIR, 'dist', krausest)} && pnpm vite preview --port ${EXPERIMENT_PORT}`;
 
 await new Promise((resolve) => {
   // giving 5 seconds for the server to start
