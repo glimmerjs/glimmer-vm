f2f862f9fea04c0e8ff3bc85fda3a012558829da
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-04-30 10:08:40 -0400

    Copy the implementations of the raimaining tracked collections from tracked-built-ins

---
+++ packages/@glimmer-workspace/integration-tests/test/collections/set-test.ts
@@ -0,0 +1,321 @@
+import { trackedSet } from '@glimmer/validator';
+import {
+  defineComponent,
+  GlimmerishComponent as Component,
+  jitSuite,
+  RenderTest,
+  test,
+} from '@glimmer-workspace/integration-tests';
+
+class TrackedSetTest extends RenderTest {
+  static suiteName = `trackedSet() (rendering)`;
+
+  @test
+  'options.equals: default equals does not dirty on no-op changes'(assert: Assert) {
+    let key = { foo: '123' };
+    const obj = trackedSet([key]);
+    const step = () => {
+      let str = String(obj.has(key));
+      assert.step(str);
+      return str;
+    };
+
+    const Foo = defineComponent({ step }, '{{(step)}}');
+
+    this.renderComponent(Foo);
+
+    this.assertHTML('true');
+    assert.verifySteps(['true']);
+
+    obj.add(key);
+    this.rerender();
+
+    this.assertHTML('true');
+    this.assertStableRerender();
+    assert.verifySteps([]);
+  }
+
+  @test
+  'options.equals: using equals can dirty on every change'(assert: Assert) {
+    let key = { foo: '123' };
+    const obj = trackedSet([key], { equals: () => false });
+    const step = () => {
+      let str = String(obj.has(key));
+      assert.step(str);
+      return str;
+    };
+
+    const Foo = defineComponent({ step }, '{{(step)}}');
+
+    this.renderComponent(Foo);
+
+    this.assertHTML('true');
+    assert.verifySteps(['true']);
+
+    obj.add(key);
+    this.rerender();
+
+    this.assertHTML('true');
+    this.assertStableRerender();
+    assert.verifySteps(['true']);
+  }
+
+  @test
+  'add/has'() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  'add/has existing value'() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet(['foo']);
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      },
+      false
+    );
+  }
+
+  @test
+  'add/has existing value (with always-dirty)'() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet(['foo'], { equals: () => false });
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  'add/has unrelated value'() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.add('bar');
+        }
+      },
+      false
+    );
+  }
+
+  @test
+  entries() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.entries();
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  keys() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.keys();
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  values() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.values();
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  forEach() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          this.set.forEach(() => {
+            /* no-op */
+          });
+          return 'test';
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  size() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet();
+
+        get value() {
+          return this.set.size;
+        }
+
+        update() {
+          this.set.add('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  delete() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet(['foo', 123]);
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.delete('foo');
+        }
+      }
+    );
+  }
+
+  @test
+  'delete unrelated value'() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet(['foo', 123]);
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.delete(123);
+        }
+      },
+      false
+    );
+  }
+
+  @test
+  clear() {
+    this.assertReactivity(
+      class extends Component {
+        set = trackedSet(['foo', 123]);
+
+        get value() {
+          return this.set.has('foo');
+        }
+
+        update() {
+          this.set.clear();
+        }
+      }
+    );
+  }
+
+  @test
+  'each: add'() {
+    this.assertEachReactivity(
+      class extends Component {
+        collection = trackedSet(['foo', 123]);
+
+        update() {
+          this.collection.add('bar');
+        }
+      }
+    );
+  }
+
+  @test
+  'each: add existing value'() {
+    this.assertEachReactivity(
+      class extends Component {
+        collection = trackedSet(['foo', 123]);
+
+        update() {
+          this.collection.add('foo');
+        }
+      }
+    );
+  }
+}
+
+jitSuite(TrackedSetTest);
+
+// TODO: These tests are currently unstable on release, turn back on once
+// behavior is fixed
+// eachInReactivityTest(
+//   'add',
+//   class extends Component {
+//     collection = trackedSet(['foo', 123]);
+//     update() {
+//       this.collection.add('bar');
+//     }
+//   }
+// );
+// eachInReactivityTest(
+//   'add existing value',
+//   class extends Component {
+//     collection = trackedSet(['foo', 123]);
+//     update() {
+//       this.collection.add('foo');
+//     }
+//   }
+// );
---
+++ packages/@glimmer/validator/lib/collections/set.ts
@@ -0,0 +1,216 @@
+import type { ReactiveOptions } from './types';
+
+import { consumeTag } from '../tracking';
+import { createUpdatableTag, DIRTY_TAG } from '../validators';
+
+class TrackedSet<T = unknown> implements Set<T> {
+  #options: ReactiveOptions<T>;
+  #collection = createUpdatableTag();
+  #storages = new Map<T, ReturnType<typeof createUpdatableTag>>();
+  #vals: Set<T>;
+
+  #storageFor(key: T): ReturnType<typeof createUpdatableTag> {
+    const storages = this.#storages;
+    let storage = storages.get(key);
+
+    if (storage === undefined) {
+      storage = createUpdatableTag();
+      storages.set(key, storage);
+    }
+
+    return storage;
+  }
+
+  #dirtyStorageFor(key: T): void {
+    const storage = this.#storages.get(key);
+
+    if (storage) {
+      DIRTY_TAG(storage);
+    }
+  }
+
+  constructor(existing: Iterable<T>, options: ReactiveOptions<T>) {
+    this.#vals = new Set(existing);
+    this.#options = options;
+  }
+
+  // **** KEY GETTERS ****
+  has(value: T): boolean {
+    consumeTag(this.#storageFor(value));
+
+    return this.#vals.has(value);
+  }
+
+  // **** ALL GETTERS ****
+  entries() {
+    consumeTag(this.#collection);
+
+    return this.#vals.entries();
+  }
+
+  keys() {
+    consumeTag(this.#collection);
+
+    return this.#vals.keys();
+  }
+
+  values() {
+    consumeTag(this.#collection);
+
+    return this.#vals.values();
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  union<U>(other: ReadonlySetLike<U>): Set<T | U> {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.union(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  intersection<U>(other: ReadonlySetLike<U>): Set<T & U> {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.intersection(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  difference<U>(other: ReadonlySetLike<U>): Set<T> {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.difference(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  symmetricDifference<U>(other: ReadonlySetLike<U>): Set<T | U> {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.symmetricDifference(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  isSubsetOf(other: ReadonlySetLike<unknown>): boolean {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.isSubsetOf(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  isSupersetOf(other: ReadonlySetLike<unknown>): boolean {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.isSupersetOf(other);
+  }
+
+  // eslint-disable-next-line
+  // @ts-ignore -- These Set types added in TS 5.5
+  isDisjointFrom(other: ReadonlySetLike<unknown>): boolean {
+    consumeTag(this.#collection);
+
+    // eslint-disable-next-line
+    // @ts-ignore -- These Set types added in TS 5.5
+    return this.#vals.isDisjointFrom(other);
+  }
+
+  forEach(fn: (value1: T, value2: T, set: Set<T>) => void): void {
+    consumeTag(this.#collection);
+
+    this.#vals.forEach(fn);
+  }
+
+  get size(): number {
+    consumeTag(this.#collection);
+
+    return this.#vals.size;
+  }
+
+  [Symbol.iterator]() {
+    consumeTag(this.#collection);
+
+    return this.#vals[Symbol.iterator]();
+  }
+
+  get [Symbol.toStringTag](): string {
+    return this.#vals[Symbol.toStringTag];
+  }
+
+  add(value: T): this {
+    /**
+     * In a WeakSet, there is no `.get()`, but if there was,
+     * we could assume it's the same value as what we passed.
+     *
+     * So for a WeakSet, if we try to add something that already exists
+     * we no-op.
+     *
+     * WeakSet already does this internally for us,
+     * but we want the ability for the reactive behavior to reflect the same behavior.
+     *
+     * i.e.: doing weakSet.add(value) should never dirty with the defaults
+     *       if the `value` is already in the weakSet
+     */
+    if (this.#vals.has(value)) {
+      /**
+       * This looks a little silly, where a always will === b,
+       * but see the note above.
+       */
+      let isUnchanged = this.#options.equals(value, value);
+      if (isUnchanged) return this;
+    } else {
+      DIRTY_TAG(this.#collection);
+    }
+
+    this.#dirtyStorageFor(value);
+
+    this.#vals.add(value);
+
+    return this;
+  }
+
+  delete(value: T): boolean {
+    this.#dirtyStorageFor(value);
+    DIRTY_TAG(this.#collection);
+
+    this.#storages.delete(value);
+    return this.#vals.delete(value);
+  }
+
+  // **** ALL SETTERS ****
+  clear(): void {
+    this.#storages.forEach((s) => DIRTY_TAG(s));
+    DIRTY_TAG(this.#collection);
+
+    this.#storages.clear();
+    this.#vals.clear();
+  }
+}
+
+// So instanceof works
+Object.setPrototypeOf(TrackedSet.prototype, Set.prototype);
+
+export function trackedSet<Value = unknown>(
+  data?: Set<Value> | Value[] | Iterable<Value> | null,
+  options?: { equals?: (a: Value, b: Value) => boolean; description?: string }
+): Set<Value> {
+  return new TrackedSet(data ?? [], {
+    equals: options?.equals ?? Object.is,
+    description: options?.description,
+  });
+}
